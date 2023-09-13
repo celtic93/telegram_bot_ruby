adfd48955e854293e1ed10d73af468e080eaf2c6
celtic93 <egorka80@list.ru>
2023-09-13 23:29:54 +0300

    Added match_update methot to DB

--- config/db.rb
+++ config/db.rb
@@ -9,11 +9,18 @@ module DB
   SCHEMA_FILE_PATH = "#{File.expand_path('..', __dir__)}/db/schema.json".freeze
 
   def read
-    JSON.parse(File.read(DATA_FILE_PATH)).symbolize_keys
+    JSON.parse(File.read(DATA_FILE_PATH)).deep_symbolize_keys
   end
 
   def write(data)
     File.write(DATA_FILE_PATH, data.to_json)
+    true
+  end
+
+  def update_match(link_path, params)
+    data = read
+    data[:matches].detect { |d| d[:link_path] == link_path }.update(params)
+    write(data)
     true
   end
 
